f9c6cfd024ec1f244fc822c6eeb763882fe870e8
AndrewGMorgan <morgan@kernel.org>
2021-06-20 19:57:52 -0700

    Create retest.sh

---
+++ retest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir clone
+git clone git://git.kernel.org/pub/scm/libs/libcap/libcap.git
+cd clone || exit 1
+HASH=$(git rev-parse HEAD)
+cd ..
+if [[ -z "${HASH}" ]]; then
+  echo "unable to determine hash value of clone"
+  exit 1
+fi
+if [[ "${HASH}" = "$(cat last-hash)" ]]; then
+  echo "no change since last clone request"
+  exit 0
+fi
+echo "should build and test clone ${HASH}"
