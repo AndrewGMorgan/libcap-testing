164ed2d690eb7c67239b685d45c56a624a8f35cc
AndrewGMorgan <morgan@kernel.org>
2021-06-21 18:12:29 -0700

    Allow for make failures.
    We've moved more smarts into the retest.sh script. Not quite ready to autotest based on upstream commits.

--- retest.sh
+++ retest.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo force a failure
-exit 1
+echo "some environment setup"
 
-HASH="$(git ls-remote --head git://git.kernel.org/pub/scm/libs/libcap/libcap.git|awk '{print $1}')"
+uname -a
+gcc --version
+go version
+id
+pwd
+
+URL=git://git.kernel.org/pub/scm/libs/libcap/libcap.git
+
+HASH="$(git ls-remote --head ${URL}|awk '{print $1}')"
 echo "should build and test clone: ${HASH}"
 if [[ -z "${HASH}" ]]; then
   echo "unable to determine hash value of upstrem"
@@ -18,17 +25,22 @@ mkdir built
 FAKEROOT="$(pwd)/built"
 mkdir clone
 cd clone || exit 1
-git clone git://git.kernel.org/pub/scm/libs/libcap/libcap.git
+git clone "${URL}"
 cd libcap || exit 1
 # redefine HASH from actual copy - avoid race condition.
 HASH=$(git rev-parse HEAD)
-if [[ "${HASH}" = "$(cat last-hash 2>/dev/null)" ]]; then
+if [[ "${HASH}" = "$(cat ../last-hash 2>/dev/null)" ]]; then
   echo "no change since last clone request"
   exit 0
 fi
+cd ../..
 
 echo "should build and test clone ${HASH}"
-make FAKEROOT="${FAKEROOT}" all test install distclean
+echo "${HASH}" > last-hash
+ls -l
+
+cd clone/libcap
+make FAKEROOT="${FAKEROOT}" all test install distclean || exit 1
 
 cd "${FAKEROOT}"
 ls -lRt
