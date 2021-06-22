158794ac4566e59b8fb63ae732fba6b75901dd2f
AndrewGMorgan <morgan@kernel.org>
2021-06-21 19:12:49 -0700

    Try to set commit id
    Working through test running problems

--- retest.sh
+++ retest.sh
@@ -11,12 +11,13 @@ pwd
 URL=git://git.kernel.org/pub/scm/libs/libcap/libcap.git
 
 HASH="$(git ls-remote --head ${URL}|awk '{print $1}')"
+LASTHASH="$(cat last-hash 2>/dev/null)"
 echo "should build and test clone: ${HASH}"
 if [[ -z "${HASH}" ]]; then
   echo "unable to determine hash value of upstrem"
   exit 1
 fi
-if [[ "${HASH}" = "$(cat last-hash 2>/dev/null)" ]]; then
+if [[ "${HASH}" = "${LASTHASH}" ]]; then
   echo "no change since last clone request"
   exit 0
 fi
@@ -29,7 +30,7 @@ git clone "${URL}"
 cd libcap || exit 1
 # redefine HASH from actual copy - avoid race condition.
 HASH=$(git rev-parse HEAD)
-if [[ "${HASH}" = "$(cat ../last-hash 2>/dev/null)" ]]; then
+if [[ "${HASH}" = "${LASTHASH}" ]]; then
   echo "no change since last clone request"
   exit 0
 fi
@@ -37,6 +38,8 @@ cd ../..
 
 echo "should build and test clone ${HASH}"
 echo "${HASH}" > last-hash
+git config user.email "morgan@kernel.org"
+git config user.name "Andrew's Robot"
 git status
 git add last-hash
 git commit -m "upstream libcap updated just before $(date)"
