eaa92b4338215de81f37d20efdefd1d1ab0ea821
Andrew G. Morgan <morgan@kernel.org>
2021-06-26 18:48:36 -0700

    explore maintaining a history of builds
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>

---
+++ build.md
@@ -0,0 +1,7 @@
+# History of automated builds of libcap
+
+This page is maintained by a robot. It captures high level summaries
+of libcap builds so far.
+
+## Builds since libcap-2.49
+
--- retest.sh
+++ retest.sh
@@ -2,6 +2,10 @@
 
 echo "some environment setup"
 
+# Clean up after last invocation. Github does this automatically, but
+# testing from my sandbox does not.
+rm -rf clone built
+
 uname -a
 gcc --version
 go version
@@ -37,18 +41,37 @@ if [[ "${HASH}" = "${LASTHASH}" ]]; then
 fi
 cd ../..
 
-echo "should build and test clone ${HASH}"
+echo "should build and test clone for all commits after"
+echo "  ${LASTHASH} ... to ..."
+echo "  ${HASH}"
+
 echo "${HASH}" > last-hash
 git config user.email "morgan@kernel.org"
 git config user.name "Andrew's Robot"
+
+cd clone/libcap
+git checkout -b rebuild 
+
+for h in $(git log --reverse HEAD..."${LASTHASH}" --pretty=format:"%H"); do
+    git reset --hard "${h}"
+    rm -rf "${FAKEROOT}/*"
+    STATUS="PASS"
+    make FAKEROOT="${FAKEROOT}" clean all test install || STATUS="FAIL"
+    (git log -1 --pretty=format:"-  %cd [%h](https://git.kernel.org/pub/scm/libs/libcap/libcap.git/commit/?id=%H): ${STATUS}" ; echo) >> ../../build.md
+done
+
+cd "${FAKEROOT}"
+ls -lRt
+
+cd ..
+(cat README.head ; echo "## Current build status: ${STATUS}" ; tail -10 build.md) > README.md
 git status
-git add last-hash
+git add last-hash build.md README.md
 git commit -m "upstream libcap updated just before $(date)"
 git status
 git push || exit 1
 
-cd clone/libcap
-make FAKEROOT="${FAKEROOT}" all test install distclean || exit 1
-
-cd "${FAKEROOT}"
-ls -lRt
+echo "Most recent build (${h}) was a ${STATUS}"
+if [[ "${STATUS}}" = "FAIL" ]]; then
+    exit 1
+fi
